f48df7bb3f55549d011be01cf80986cb79b3cc96
aymene <aymene.mazouz@lacatholille.fr>
2024-08-28 23:38:24 +0200

    initial commit

---
+++ src/config/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBAb8nCEE2fWG0EFbGdg84vualnXtb1B-k",
+    authDomain: "studlink-v2.firebaseapp.com",
+    projectId: "studlink-v2",
+    storageBucket: "studlink-v2.appspot.com",
+    messagingSenderId: "229233904702",
+    appId: "1:229233904702:web:2631255d33c0488c1a556f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
